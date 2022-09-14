0003952cc81c0c8f2f0c6362e5d3696d9a07538f
狐獴君 <humengsir@yeah.net>
2022-09-14 23:30:09 +0800

    0004: Add search function about article list page.

--- app/model/ArticleModel.php
+++ app/model/ArticleModel.php
@@ -9,6 +9,13 @@ class ArticleModel extends Model
 
     public function list_data(array $where = [])
     {
-        return self::where($where)->field("id,uid,cat_id,title,content,read_num,add_time,update_time,is_delete")->order('add_time', 'desc')->paginate(10, true);
+        $model = self::field("id,uid,cat_id,title,content,read_num,add_time,update_time,is_delete");
+        if (isset($where['find_in_set'])) {
+            foreach ($where['find_in_set'] as $field_name => $value) {
+                $model = $model->where($field_name, 'find in set', $value);
+            }
+            unset($where['find_in_set']);
+        }
+        return $model->where($where)->order('add_time', 'desc')->paginate(1, true);
     }
 }
